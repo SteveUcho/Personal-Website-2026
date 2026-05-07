d064f7ed069161fce73611ddf0e62dc5015c89a6
steveucho <steve.ucho8@gmail.com>
2026-05-07 19:28:46 -0400

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Personal-Website-2026",
   plugins: [
     react(),
     tailwindcss(),
